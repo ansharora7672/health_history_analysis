d9153bf3b5c1aa0e57e505c7fa5115517c4ede3a
Ansh Arora <124551418+ansharora7672@users.noreply.github.com>
2025-03-02 01:54:53 -0500

    first commit with login and signup working

---
+++ hackathon_conhack/client/clientside/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import login from './components/login'
+
+function App() {
+  return (
+    <div>
+      <p>this is something</p>
+      <login />
+      <p>this is after login</p>
+    </div>
+  )
+}
+
+export default App
---
+++ hackathon_conhack/client/clientside/src/components/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className="bg-black h-5">this is login page</div>
+  )
+}
+
+export default login
